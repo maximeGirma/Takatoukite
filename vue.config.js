83079798b92111fd667b10f53f2179ec53f61757
MaximeGirma <mgi@ucar.fr>
2019-04-02 14:03:07 +0200

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
-// config/index.js
-
 module.exports = {
-    dev: {
-// Paths
-assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
-
-// Various Dev Server settings
-    host: 'awesome-dev.eu',
-    port: 8090,
-
-// skipping other options as they are only convenience features
-},
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/team42/TAKATOUKITE/dist/'
+        : '/'
 }
