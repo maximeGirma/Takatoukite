c67b91ba21d6db288888eb829e227bc6befe998c
MaximeGirma <mgi@ucar.fr>
2019-04-02 14:00:23 +0200

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 // config/index.js
-'use strict'
-const path = require('path')
 
 module.exports = {
     dev: {
@@ -14,18 +12,5 @@ assetsSubDirectory: 'static',
     port: 8090,
 
 // skipping other options as they are only convenience features
-},
-build: {
-    // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
-
-        // Paths
-        assetsRoot: path.resolve(__dirname, '../dist'),
-        assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
-
-        productionSourceMap: true,
-
-    // skipping the rest ...
 },
 }
